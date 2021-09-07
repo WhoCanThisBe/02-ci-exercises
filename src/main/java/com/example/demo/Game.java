b144819c34c5db0e71b18eb6ac4301f6c58c55a5
WhoCanThisBe <Wen.hhp@gmail.com>
2021-09-07 11:13:08 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/Game.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+/**
+ * Exercise Implement the rest of the code neccessary
+ */
+public class Game {
+
+    void roll(int pins) {
+    }
+
+    int score() {
+        return 0;
+    }
+
+}
